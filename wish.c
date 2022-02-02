e574778635352d592d3e77f57468658f5691dd3e
2019331101-Tanjumul <Crackerroot4@gmail.com>
2022-02-02 12:20:34 +0600

    First commit

---
+++ wish.c
@@ -0,0 +1,21 @@
+#include<stdio.h> 
+int main () 
+{
+int i ,full; 
+printf("Give here your age here:\n");
+scanf("%d",&full);
+
+for(i=1 ; i<=full; i++){
+printf("Happy birthday to you when you are %d",i);
+  
+ }
+
+ //That means, i'm wishing you since your first birthdate !!
+
+return 0; 
+
+}
+
+ 
+
+
